6f6c1d121b175151ba8f207014d6f8b7817f357b
haonhat0703 <haonhat2729@gmail.com>
2022-08-25 23:46:37 +0700

    verifyToken to know use login or not (jsonwebtoken) save cookie

--- middleware/index.js
+++ middleware/index.js
@@ -2,6 +2,8 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
+  // const token =
+  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzA2MmViOWQ4YWVhMGQ1YTFmMzc2ZGUiLCJpYXQiOjE2NjE0MTg3OTl9.XWnjUmpVXgZEPDv_bKdQ7_etw8MUuUOEuNC_pttb85s";
 
   if (!token) {
     res.redirect("/login");
